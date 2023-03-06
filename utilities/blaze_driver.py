2e84a5c63892de0e98e804758d537f625a498829
AlexZak17 <zak6598@gmail.com>
2023-03-06 15:04:25 +0200

    v1

---
+++ utilities/blaze_driver.py
@@ -0,0 +1,18 @@
+from webdriver_manager.chrome import ChromeDriverManager
+import unittest
+from time import sleep
+import HtmlTestRunner
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox import options as O
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common import action_chains as AC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service
+
+my_driver_chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
