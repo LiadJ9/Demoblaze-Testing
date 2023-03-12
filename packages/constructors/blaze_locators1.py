18347f25b85d8fce3160f1b0327f7eda817f5a59
AlexZak17 <zak6598@gmail.com>
2023-03-12 18:43:10 +0200

    updates

--- packages/constructors/blaze_locators1.py
+++ packages/constructors/blaze_locators1.py
@@ -1,4 +1,4 @@
-import utilities.blaze_driver as bu
+import packages.utilities.blaze_driver as bu
 # MainPageLocators = MPL
 # HeaderLocators = HL
 
@@ -15,28 +15,39 @@ import utilities.blaze_driver as bu
 # PB = previous_button
 # FPI = first_page_image
 
+# CB = Cart Button
+# LHB = LogIn header Button
+# UF = Username Field
+# PF = Password Field
+# LB = Login Button
+# RP = Random Product
+
 # # # # # LOCATORS - We use these to tidy up our code and save time searching for elements # # # # # #
 
 
 class HL(object):
-    home_button = (bu.By.XPATH, '/html/body/nav/div[1]/ul/li[1]/a')
-    hb = '/html/body/nav/div[1]/ul/li[1]/a'
-    FBHP = '/html/body/nav/div[2]/div/ol/li[1]'
-    FI = '/html/body/nav/div[2]/div/ol/li[1]'
-    SBHP = '/html/body/nav/div[2]/div/ol/li[2]'
-    SI = '/html/body/nav/div[2]/div/div/div[2]/img'
-    TBHP = '/html/body/nav/div[2]/div/ol/li[3]'
-    TI = '/html/body/nav/div[2]/div/div/div[3]/img'
+    hb = (bu.By.XPATH, '/html/body/nav/div[1]/ul/li[1]/a')
+    FBHP = (bu.By.XPATH, '/html/body/nav/div[2]/div/ol/li[1]')
+    FI = (bu.By.XPATH, '/html/body/nav/div[2]/div/ol/li[1]')
+    SBHP = (bu.By.XPATH, '/html/body/nav/div[2]/div/ol/li[2]')
+    SI = (bu.By.XPATH, '/html/body/nav/div[2]/div/div/div[2]/img')
+    TBHP = (bu.By.XPATH, '/html/body/nav/div[2]/div/ol/li[3]')
+    TI = (bu.By.XPATH, '/html/body/nav/div[2]/div/div/div[3]/img')
 
 class MPL(object):
-    BCP = '/html/body/nav/div[2]/div/a[2]'
-    second_pic = '/html/body/nav/div[2]/div/div/div[2]/img'
-    NB = '/html/body/div[5]/div/div[2]/form/ul/li[2]/button'
-    mc_img = '/html/body/div[5]/div/div[2]/div/div[1]/div/a/img'
-    PB = '/html/body/div[5]/div/div[2]/form/ul/li[1]/button'
-    FPI = '/html/body/div[5]/div/div[2]/div/div[1]/div/a/img'
+    BCP = (bu.By.XPATH, '/html/body/nav/div[2]/div/a[2]')
+    second_pic = (bu.By.XPATH, '/html/body/nav/div[2]/div/div/div[2]/img')
+    NB = (bu.By.XPATH, '/html/body/div[5]/div/div[2]/form/ul/li[2]/button')
+    mc_img = (bu.By.XPATH, '/html/body/div[5]/div/div[2]/div/div[1]/div/a/img')
+    PB = (bu.By.XPATH, '/html/body/div[5]/div/div[2]/form/ul/li[1]/button')
+    FPI = (bu.By.XPATH, '/html/body/div[5]/div/div[2]/div/div[1]/div/a/img')
 
 
 
-class CartPageLocators(object):
-    pass
+class Cart(object):
+    CB = (bu.By.XPATH, '/html/body/nav/div[1]/ul/li[5]/a')
+    LHB = (bu.By.XPATH, '/html/body/nav/div[1]/ul/li[5]/a')
+    UF = (bu.By.ID, 'loginusername')
+    PF = (bu.By.ID, 'loginpassword')
+    LB = (bu.By.XPATH, '//*[@id="logInModal"]/div/div/div[3]/button[2]')
+    RP = (bu.By.XPATH, '//*[@id="tbodyid"]/div[1]/div/div/h4/a')
--- test_demoblaze_chrome.py
+++ test_demoblaze_chrome.py
@@ -1,6 +1,7 @@
-import constructors.blaze_locators as bl
-import utilities.blaze_driver as bu
+import packages.constructors.blaze_locators1 as bl1
+import packages.utilities.blaze_driver as bu
 import HtmlTestRunner
+import packages.pages.blaze_pages1 as bp1
 
 
 class TestBlazeMain(bu.unittest.TestCase):
@@ -11,57 +12,75 @@ class TestBlazeMain(bu.unittest.TestCase):
         self.my_driver.maximize_window()
 
     def test_Home_button(self):
-        bu.WDW(self.my_driver, 10).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        home = self.my_driver.find_element(bu.By.XPATH, bl.HL.hb)
+        bu.WDW(self.my_driver, 10).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        home = self.my_driver.find_element(*bl1.HL.hb)
         home.click()
-        assert self.my_driver.find_element(bu.By.XPATH, bl.HL.hb)
+        assert self.my_driver.find_element(*bl1.HL.hb)
 
     def test_BCP(self):
-        bu.WDW(self.my_driver, 10).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        home = self.my_driver.find_element(bu.By.XPATH, bl.HL.hb)
+        bu.WDW(self.my_driver, 10).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        home = self.my_driver.find_element(*bl1.HL.hb,)
         home.click()
-        change_picture = self.my_driver.find_element(bu.By.XPATH, bl.MPL.BCP)
+        change_picture = self.my_driver.find_element(bu.By.XPATH, bl1.MPL.BCP)
         change_picture.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.MPL.second_pic)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.MPL.second_pic)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl1.MPL.second_pic)))
+        assert self.my_driver.find_element(bu.By.XPATH, bl1.MPL.second_pic)
 
     def test_button_next(self):
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        b_next = self.my_driver.find_element(bu.By.XPATH, bl.MPL.NB)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        b_next = self.my_driver.find_element(*bl1.MPL.NB,)
         b_next.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.MPL.mc_img)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.MPL.mc_img)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.MPL.mc_img,)))
+        assert self.my_driver.find_element(*bl1.MPL.mc_img,)
 
     def test_button_previous(self):
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        next_b = self.my_driver.find_element(bu.By.XPATH, bl.MPL.NB)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        next_b = self.my_driver.find_element(*bl1.MPL.NB,)
         next_b.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.MPL.mc_img)))
-        previous_b = self.my_driver.find_element(bu.By.XPATH, bl.MPL.PB)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.MPL.mc_img,)))
+        previous_b = self.my_driver.find_element(*bl1.MPL.PB,)
         previous_b.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.MPL.FPI)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.MPL.FPI)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.MPL.FPI,)))
+        assert self.my_driver.find_element(*bl1.MPL.FPI,)
 
     def test_first_button_header_picture(self):
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        first_button = self.my_driver.find_element(bu.By.XPATH, bl.HL.FBHP)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        first_button = self.my_driver.find_element(*bl1.HL.FBHP,)
         first_button.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.FI)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.HL.FI)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.FI,)))
+        assert self.my_driver.find_element(*bl1.HL.FI,)
 
     def test_second_button_header_picture(self):
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        second_button = self.my_driver.find_element(bu.By.XPATH, bl.HL.SBHP)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        second_button = self.my_driver.find_element(*bl1.HL.SBHP,)
         second_button.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.SI)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.HL.SI)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.SI,)))
+        assert self.my_driver.find_element(*bl1.HL.SI,)
 
     def test_third_button_header_picture(self):
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.hb)))
-        third_button = self.my_driver.find_element(bu.By.XPATH, bl.HL.TBHP)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.hb,)))
+        third_button = self.my_driver.find_element(*bl1.HL.TBHP,)
         third_button.click()
-        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((bu.By.XPATH, bl.HL.TI)))
-        assert self.my_driver.find_element(bu.By.XPATH, bl.HL.TI)
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.HL.TI,)))
+        assert self.my_driver.find_element(*bl1.HL.TI,)
+
+    def test_add_item_to_cart_while_LoggedIn(self):
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.Cart.LHB,)))
+        LogIn_B = self.my_driver.find_element(*bl1.Cart.LHB,)
+        LogIn_B.click()
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.Cart.UF,)))
+        UNF = self.my_driver.find_element(*bl1.Cart.UF,)
+        UNF.clear()
+        UNF.send_keys('alex12345')
+        PF = self.my_driver.find_element(*bl1.Cart.PF,)
+        PF.clear()
+        PF.send_keys('12345')
+        LB = self.my_driver.find_element(*bl1.Cart.LB,)
+        LB.click()
+        bu.WDW(self.my_driver, 5).until(bu.EC.visibility_of_element_located((*bl1.Cart.RP,)))
+        RP = self.my_driver.find_element(*bl1.Cart.RP,)
+        RP.click()
+
 
     def tearDown(self):
         bu.sleep(3)
